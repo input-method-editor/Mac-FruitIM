6636170c659621ebfa2b640f13877d7958d87350
Chi-En Wu <jason2506.mail@gmail.com>
2012-01-29 03:32:34 +0800

    Don't display the candidates window if there is only one candidate to select.

--- FruitIM/InputMethodController.m
+++ FruitIM/InputMethodController.m
@@ -51,7 +51,7 @@ static const KeyCode
 
 - (BOOL) _shouldIgnoreKey:(NSInteger)keyCode modifiers:(NSUInteger)flags;
 - (BOOL) _handleOperationKey:(NSInteger)keyCode client:(id)client;
-- (void) _showCandidates;
+- (BOOL) _showCandidates;
 - (void) _updateComposition:(id)client;
 
 @end
@@ -195,7 +195,7 @@ static const KeyCode
             break;
 
         case KEY_MOVE_DOWN:
-            [self _showCandidates];
+            isPassed = [self _showCandidates];
             break;
 
         default:
@@ -208,10 +208,14 @@ static const KeyCode
     return YES;
 }
 
-- (void) _showCandidates
+- (BOOL) _showCandidates
 {
+    if (_buffer.candidates.count == 1)
+        return NO;
+
     [sharedCandidates updateCandidates];
     [sharedCandidates show:kIMKLocateCandidatesBelowHint];
+    return YES;
 }
 
 - (void) _updateComposition:(id)client
